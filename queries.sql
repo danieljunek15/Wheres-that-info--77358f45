90044370c527025ca4673a909a1d14c594b40038
unknown <dvandenbrink@roc-dev.com>
2020-03-05 13:54:02 +0100

    Uitwerking van de exercise

--- queries.sql
+++ queries.sql
@@ -4,5 +4,5 @@ SELECT * FROM netland.series WHERE country = 'NL' AND language = 'NL';
 SELECT * FROM netland.series WHERE seasons BETWEEN '5' AND '50';
 SELECT * FROM netland.series ORDER BY rating;
 SELECT * FROM netland.series WHERE seasons < 3 OR seasons > 20;
-SELECT * FROM netland.series WHERE title LIKE 'Th%';
+SELECT * FROM netland.series WHERE title LIKE '%Th%';
 SELECT * FROM netland.series WHERE seasons <> 3;
